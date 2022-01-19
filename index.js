d184ff52b91141ac69f93958faa0da2e2c59848f
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-01-18 21:49:10 -0300

    Desafio 3 Done

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ app.use(bodyParser.json());
 
 const getTalker = require('./middlewares/getTalker');
 const getTalkerById = require('./middlewares/getTalkerById');
+const { isValidEmail, isValidPassword } = require('./middlewares/validations');
+const token = require('./middlewares/generateTolken');
 
 app.get('/talker', getTalker);
 app.get('/talker/:id', getTalkerById);
+app.post('/login', isValidEmail, isValidPassword, (_req, res) => res.status(200).json({ token: token() }));
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
